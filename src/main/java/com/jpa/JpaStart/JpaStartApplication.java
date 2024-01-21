1d19c8f7f98f7489ede42026f2a52a969ff7daf5
hwlee9905 <9905hyun@naver.com>
2024-01-22 00:23:25 +0900

    practice update

--- src/main/java/com/jpa/JpaStart/JpaStartApplication.java
+++ src/main/java/com/jpa/JpaStart/JpaStartApplication.java
@@ -1,13 +1,59 @@
 package com.jpa.JpaStart;
 
+import com.jpa.model.Member;
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class JpaStartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaStartApplication.class, args);
-	}
 
+		SpringApplication.run(JpaStartApplication.class, args);
+		//엔티티 매니저 팩토리 생성
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpastart");
+		EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+		EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+		try {
+			tx.begin(); //트랜잭션 시작
+			logic(em);  //비즈니스 로직
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback(); //트랜잭션 롤백
+		} finally {
+			em.close(); //엔티티 매니저 종료
+		}
+		emf.close(); //엔티티 매니저 팩토리 종료
+	}
+	public static void logic(EntityManager em) {
+
+		String id = "id1";
+		Member member = new Member();
+		member.setId(id);
+		member.setUsername("지한");
+		member.setAge(2);
+
+		//등록
+		em.persist(member);
+
+		//수정
+		member.setAge(20);
+
+		//한 건 조회
+		Member findMember = em.find(Member.class, id);
+		System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
+
+		//목록 조회
+		List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+		System.out.println("members.size=" + members.size());
+
+		//삭제
+		em.remove(member);
+		em.setFlushMode(FlushModeType.COMMIT);
+	}
 }
