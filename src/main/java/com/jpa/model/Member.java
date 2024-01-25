432aad9a6a1b20dec81b8f3cfb6374dd4cefcd7d
hwlee9905 <9905hyun@naver.com>
2024-01-25 10:43:13 +0900

    practice commit

--- src/main/java/com/jpa/model/Member.java
+++ src/main/java/com/jpa/model/Member.java
@@ -4,15 +4,37 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
+import java.util.Date;
+
 @Entity
-@Table(name = "MEMBER")
+@Table(name="MEMBER", uniqueConstraints = {@UniqueConstraint( //추가 //**
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"} )})
 public class Member {
     @Id
     @Column(name = "ID")
     private String id;
-    @Column(name = "NAME")
+
+    @Column(name = "NAME", nullable = false, length = 10) //추가 //**
+//    @Column(name = "NAME") //추가 //**
     private String username;
-    //매핑 정보가 없는 필드
+
     private Integer age;
+
+    //=== 추가
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private String temp;
+
 }
